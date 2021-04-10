e31fa5a32e99089defe8c7a3e52b7b52c5ce0ed3
Anastasia Titova <nastyatitova3538@gmail.com>
2021-04-10 11:31:04 -0700

    set up layout for sidenav

--- src/layout/Sidenav.js
+++ src/layout/Sidenav.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import styled from 'styled-components';
+import logo from '../images/logo.svg'
+
+// Components
+import Text from '../components/Text'
 
 const Sidenav = () => {
     return (
-        <div>
-            Enter
-        </div>
+        <Section>
+            <Wrapper>
+                <Logo src={logo} alt="pets paw" />
+                <div>
+                    <h1>Hi Intern!</h1>
+                    <Text sm gray content="Welcome to MSI 2021 Front-end test" />
+                    <h3>Lets start using The Dogs API</h3>
+                </div>
+                
+            </Wrapper>
+        </Section>
     )
 }
 
 export default Sidenav
+
+
+const Section = styled.section`
+    background: ${props => props.theme.bgMain};
+    height: 100vh;
+    width: 50%;
+`
+
+const Wrapper = styled.div`
+    margin: 2rem 6rem;
+`
+
+const Logo = styled.img`
+    width: 8rem;
+    height: auto;
+`
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,14 +1,16 @@
 export const lightTheme = {
-    heading: "rgba(29, 29, 29, 1)",
-    text: "rgba(140, 140, 140, 1)",
-    bgMain: "#E5E5E5;",
+    textPrim: "#1D1D1D", // black
+    textSec: "#8C8C8C", // gray
+
+    bgMain: "#F8F8F7;",
     bgSide: "#E5E5E5",
     bgPink: "#FBE0DC",
 
   };
   
 export const darkTheme = {
-    heading: 'white',
+    textPrim: "#FFFFFF", // white
+    textSec: "#8C8C8C", // gray
 
 }
   
