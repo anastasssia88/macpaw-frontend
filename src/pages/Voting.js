af9cff3ab9737b2fe21785603d945e391b14826a
Anastasia Titova <nastyatitova3538@gmail.com>
2021-04-19 07:22:10 -0700

    set up different fav btn for favorites/not favorites

--- src/pages/Voting.js
+++ src/pages/Voting.js
@@ -12,13 +12,16 @@ import ActionLog from '../layout/ActionLog'
 
 
 const Voting = ({ like, fav, disl }) => { 
+    // Shared state
     const { likeKey, favKey, disKey } = useContext( DogContext )
     const [ liked, addToLiked ] = likeKey
     const [ favorites, addToFav] = favKey
     const [ disliked, addToDisliked ] = disKey
-    
+
+    // Local state with data from dog's api
     const [ randomDog, setRandomDog ] = useState({})
 
+    // Fetching data from dogapi on changes in state liked, disliked
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios('https://api.thedogapi.com/v1/images/search');
@@ -30,12 +33,22 @@ const Voting = ({ like, fav, disl }) => {
     const url = randomDog.url
     const id = randomDog.id
 
-    
+    // Setting favorite icon filled/not filled
+    const [ active, setActive ] = useState(false)
+    let path
+    if (active) { 
+        path = <path d="M8.07107 2C3.61354 2 0 5.61354 0 10.0711C0 12.2116 0.850339 14.2646 2.36396 15.7782L14.2929 27.7071C14.6834 28.0976 15.3166 28.0976 15.7071 27.7071L27.636 15.7782C29.1497 14.2646 30 12.2116 30 10.0711C30 5.61354 26.3865 2 21.9289 2C19.7884 2 17.7354 2.85034 16.2218 4.36396L15 5.58579L13.7782 4.36396C12.2646 2.85034 10.2116 2 8.07107 2Z" fill="#FFFFFF"></path>
+    } else {
+        path = <path d="M8.07107 2C4.71811 2 2 4.71811 2 8.07107C2 9.68122 2.63963 11.2254 3.77817 12.364L15 23.5858L26.2218 12.364C27.3604 11.2254 28 9.68121 28 8.07107C28 4.71811 25.2819 2 21.9289 2C20.3188 2 18.7746 2.63963 17.636 3.77817L15.7071 5.70711C15.3166 6.09763 14.6834 6.09763 14.2929 5.70711L12.364 3.77818C11.2254 2.63963 9.68121 2 8.07107 2ZM0 8.07107C0 3.61354 3.61354 0 8.07107 0C10.2116 0 12.2646 0.850343 13.7782 2.36396L15 3.58579L16.2218 2.36396C17.7354 0.850341 19.7884 0 21.9289 0C26.3865 0 30 3.61354 30 8.07107C30 10.2116 29.1497 12.2646 27.636 13.7782L15.7071 25.7071C15.3166 26.0976 14.6834 26.0976 14.2929 25.7071L2.36396 13.7782C0.850339 12.2646 0 10.2116 0 8.07107Z" fill="#FFFFFF"></path>
+    }
+
+    // Handling click events
     const handleLike = (randomDog) => {
         addToLiked(prevLiked => [...prevLiked, randomDog])
     }
 
     const handleFav = (randomDog) => {
+        setActive(!active)
         addToFav(prevFavorites => [...prevFavorites, randomDog])
     }
 
@@ -60,7 +73,7 @@ const Voting = ({ like, fav, disl }) => {
                         </ActionBtn>
                         <ActionBtn fav onClick={() => handleFav(randomDog)} >
                             <SVG viewBox="0 0 30 26"> 
-                                <path d="M8.07107 2C4.71811 2 2 4.71811 2 8.07107C2 9.68122 2.63963 11.2254 3.77817 12.364L15 23.5858L26.2218 12.364C27.3604 11.2254 28 9.68121 28 8.07107C28 4.71811 25.2819 2 21.9289 2C20.3188 2 18.7746 2.63963 17.636 3.77817L15.7071 5.70711C15.3166 6.09763 14.6834 6.09763 14.2929 5.70711L12.364 3.77818C11.2254 2.63963 9.68121 2 8.07107 2ZM0 8.07107C0 3.61354 3.61354 0 8.07107 0C10.2116 0 12.2646 0.850343 13.7782 2.36396L15 3.58579L16.2218 2.36396C17.7354 0.850341 19.7884 0 21.9289 0C26.3865 0 30 3.61354 30 8.07107C30 10.2116 29.1497 12.2646 27.636 13.7782L15.7071 25.7071C15.3166 26.0976 14.6834 26.0976 14.2929 25.7071L2.36396 13.7782C0.850339 12.2646 0 10.2116 0 8.07107Z"></path>
+                                { path }
                             </SVG>
                         </ActionBtn>
                         <ActionBtn disl onClick={() => handleDislike(randomDog)} >
