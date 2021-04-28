e1008897607da79e801c080408474f83a1514397
Anastasia Titova <nastyatitova3538@gmail.com>
2021-04-28 10:59:27 -0700

    added meta tags

--- src/routes/Gallery/Gallery.js
+++ src/routes/Gallery/Gallery.js
@@ -87,7 +87,7 @@ const Gallery = () => {
       <Search />
       <Wrapper>
         <Container>
-          <GoBack btnContent="Galery" /> 
+          <GoBack btnContent="Gallery" /> 
           <Upload onClick={() => setUploadOpen(true)}>
             <svg viewBox="0 0 16 16">
               <path d="M7.86601 0L12.2355 4.03339L11.4129 4.92452L8.48919 2.22567V12.3618H7.27645V2.30464L4.67336 4.90772L3.81583 4.05019L7.86601 0ZM1.21274 14.7873V7.51081H0V16H15.7656V7.51081H14.5529V14.7873H1.21274Z"></path>
